03e64452db0c3de77c3a43af5df5d2b4ba76a671
Francisco M.C.M. <fcasmen658@gmail.com>
2024-06-22 15:55:40 +0200

    Ajustes menores

--- src/TiradaDeDados.java
+++ src/TiradaDeDados.java
@@ -1,10 +1,12 @@
 public class TiradaDeDados {
     public static void main(String[] args) {
+
     // TIRADA DE DADOS
         int dado1 = (int) (Math.random() * 6) + 1;
         int dado2 = (int) (Math.random() * 6) + 1;
-        System.out.println("El valor de los dados es:" + "Dado 1: " + dado1 + " Dado 2: " + dado2);
+        System.out.println("El valor de los dados es:\n" + "\tDado 1: " + dado1 + "\n" + "\tDado 2: " + dado2);
         System.out.println("");
+
     // TIRADA DE DADOS ENTRE 1 Y 6
         int valorEntero = (int) (Math.floor(Math.random()*(1-6+1)+6));  // Valor entre M y N, ambos incluidos.
         System.out.println("El valor aleatorio es: " + valorEntero);
